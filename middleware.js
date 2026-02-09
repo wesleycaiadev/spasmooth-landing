04efe4f1471913710f6ec59c3c84464aa66e0b27
wesleycaiadev <wesleycaia.dev@gmail.com>
2026-02-09 20:01:47 -0300

    chore: update clerk and fix middleware matcher

--- middleware.js
+++ middleware.js
@@ -4,9 +4,9 @@ export default clerkMiddleware();
 
 export const config = {
     matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
+        // Pula arquivos internos do Next.js e arquivos estáticos
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
+        // Sempre roda para rotas de API
         '/(api|trpc)(.*)',
     ],
 };
