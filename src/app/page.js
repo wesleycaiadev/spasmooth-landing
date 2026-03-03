f5d4e74cece741d8477d82ab1620b803117088d7
wesleycaiadev <wesleycaia.dev@gmail.com>
2026-03-03 12:54:29 -0300

    style(landing): move LocationSection below BookingSection and fix client component

--- src/app/page.js
+++ src/app/page.js
@@ -18,9 +18,9 @@ export default function Home() {
       <Hero />
       <Services />
       <BookingSection />
+      <LocationSection />
       <Testimonials />
       <FAQ />
-      <LocationSection />
       <Footer />
       <WhatsAppFloat />
     </main>
