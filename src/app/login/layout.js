537dbd48018f8e507ab871e380e4a3c7c9056260
Wesley Caiã <wesleycaia.dev@gmail.com>
2026-04-10 23:41:47 -0300

    chore: codebase cleanup and removal of deprecated routes/assets

--- src/app/login/layout.js
+++
@@ -1,9 +0,0 @@
-import { ClerkProvider } from "@clerk/nextjs";
-
-export default function LoginLayout({ children }) {
-    return (
-        <ClerkProvider>
-            {children}
-        </ClerkProvider>
-    );
-}
